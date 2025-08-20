9d8dcb402d5b78a9c5396d71d20037117f90ec5e
RAGAV-24 <ragavrunner33@gmail.com>
2025-08-20 13:56:35 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="p-10 ">
+<p className="text-amber-300"> DEVA</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
